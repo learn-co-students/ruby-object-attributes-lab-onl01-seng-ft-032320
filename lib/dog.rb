762fc39a798d649235bbb77feefb7cdfbb9c1cbc
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-30 15:53:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  def name=(new_name) #setter
+    @name = new_name
+  end 
+  
+  def name # getter
+    @name
+  end
+  
+  def breed=(new_breed) #setter
+    @breed = new_breed
+  end 
+  
+  def breed 
+    @breed
+  end 
+end 
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  
+  def name=(new_name) #setter
+    @name = new_name
+  end 
+  
+  def name # getter
+    @name
+  end
+  
+  def job=(new_job) #setter
+    @job = new_job
+  end 
+  
+  def job 
+    @job
+  end 
+end 
